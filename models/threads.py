7c83ac31d2e98384defbd033baaa47e4a10057f4
Aleksa <some12curious@gmail.com>
2018-09-27 01:43:19 +0200

    Implementing "loadNext" and "loadPrevious", also added "pageLength" method and "replaceData" method.

--- models/threads.py
+++ models/threads.py
@@ -57,6 +57,12 @@ class ThreadsListModel(QAbstractListModel):
         self._displayed_data = self._data[self.begin:self.end]
         self.endResetModel()
 
+    def replaceData(self, data):
+        self.beginResetModel()
+        self._data = data
+        self._displayed_data = self._data[self.begin:self.end]
+        self.endResetModel()
+
     def extractId(self, index):
         return self._displayed_data[index.row()].id
 
@@ -66,7 +72,39 @@ class ThreadsListModel(QAbstractListModel):
         return False
 
     def loadNext(self):
-        pass
+        self.begin += self.PER_PAGE
+        self.end += self.PER_PAGE
+        if self.begin > len(self._data) - self.PER_PAGE:
+            if self.end >= len(self._data) and self.begin < len(self._data):
+                self.begin = self.begin
+            elif self.end >= len(self._data) and self.begin >= len(self._data):
+                self.begin -= self.PER_PAGE
+            else:
+                self.begin = len(self._data) - self.PER_PAGE
+        if self.end > len(self._data):
+            self.end = len(self._data)
+
+        self.beginResetModel()
+        self._displayed_data = self._data[self.begin:self.end]
+        self.endResetModel()
+        print(self.begin, self.end)
 
     def loadPrevious(self):
-        pass
+        self.end = self.begin
+        if self.end < self.PER_PAGE:
+            if len(self._data) >= self.PER_PAGE:
+                self.end = self.PER_PAGE
+            else:
+                self.end = len(self._data)
+
+        self.begin -= self.PER_PAGE
+        if self.begin < 0:
+            self.begin = 0
+
+        self.beginResetModel()
+        self._displayed_data = self._data[self.begin:self.end]
+        self.endResetModel()
+        print(self.begin, self.end)
+
+    def pageLength(self):
+        return len(self._displayed_data)
