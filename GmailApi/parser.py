2af4df4544aa096c1a38be2a17a29810dd56aadc
Aleksa <some12curious@gmail.com>
2018-09-26 15:50:22 +0200

    parser.py added for extracting body part/parts from email messages.

---
+++ GmailApi/parser.py
@@ -0,0 +1,31 @@
+from mailparser.mailparser import MailParser
+from base64 import urlsafe_b64decode
+
+
+class NoBoundaryMailParser(MailParser):
+
+    @property
+    def body(self):
+        return self.text_plain, self.text_html
+
+
+def extract_body(raw_message):
+    # urlsafe_b64decode returns bytes, so you need to decode it.
+    # And sometimes when decoding you can encounter weird characters,
+    # so add argument errors='replace'
+    email = urlsafe_b64decode(raw_message['raw']).decode('utf-8', errors='replace')
+
+    mail_parser = NoBoundaryMailParser.from_string(email)
+
+    try:
+        # mailparser docs: https://github.com/SpamScope/mail-parser/
+        plain, html = mail_parser.body
+        if html:
+            print('::return html part::')
+            return "\n".join(html)
+        else:
+            print('::return plain part::')
+            return plain[1]
+    except Exception as err:
+        print('Extracting body with "mailparser" failed.', str(err))
+        return mail_parser
