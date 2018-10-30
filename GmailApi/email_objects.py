b9caf521695d6b8a3e3f5397d4a2e53ce2a9f6c8
Aleksa <some12curious@gmail.com>
2018-10-30 13:34:42 +0100

    Invalid comment

--- GmailApi/email_objects.py
+++ GmailApi/email_objects.py
@@ -20,7 +20,7 @@ class MessageObject(object):
         self.thread_id = message_dict['threadId']
         self.label_ids = message_dict['labelIds']
         self.history_id = message_dict['historyId']
-        # The internal message creation timestamp in milliseconds
+        # message_dict['internalDate'] - The internal message creation timestamp in milliseconds
         self.internalDate = datetime.fromtimestamp(int(message_dict['internalDate']) / 1000)
 
     def raw(self, resource):
