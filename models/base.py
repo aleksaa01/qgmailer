c3fa6b1fb3dcb7f47aa0372e2e643a2bca527c19
Aleksa <some12curious@gmail.com>
2018-11-14 15:25:50 +0100

    models directory now has base.py that contains BaseListModel - Base class that you should inherit from when you are making you own model. existing models were redesigned to use this new base class.

---
+++ models/base.py
@@ -0,0 +1,94 @@
+from PyQt5.QtCore import QAbstractListModel, Qt, pyqtSignal
+
+from options import Options
+
+
+class BaseListModel(QAbstractListModel):
+
+    PER_PAGE = 0
+    indexesChanged = pyqtSignal(int, int)
+
+    def __init__(self, data=None, parent=None):
+        super().__init__(parent)
+
+        self._data = data if data else []
+        self.begin = 0
+        self.end = self.PER_PAGE
+        self._displayed_data = self._data[self.begin:self.end]
+
+    def rowCount(self, parent=None):
+        return len(self._displayed_data)
+
+    def displayedIndex(self):
+        # this method might be useful for pagination and it would
+        # be more useful outisde of this model.
+        return self.begin, self.end
+
+    def data(self, index, role=None):
+        raise NotImplementedError('data method is not implemented yet.')
+
+    def flags(self, index):
+        return Qt.ItemIsEnabled | Qt.ItemIsSelectable
+
+    def createData(self, data):
+        # Use only if no widgets are connected to this model,
+        # if you have connected widgets use "addData" instead.
+        self._data = data
+        self._displayed_data = self._data[self.begin:self.end]
+
+    def addData(self, data):
+        # Adds data to already existing data, so model reset is required.
+        self.beginResetModel()
+        self._data = data + self._data
+        self._displayed_data = self._data[self.begin:self.end]
+        self.endResetModel()
+        self.indexesChanged.emit(self.begin, self.end)
+
+    def replaceData(self, data):
+        self.beginResetModel()
+        self._data = data
+        self._displayed_data = self._data[self.begin:self.end]
+        self.endResetModel()
+        self.indexesChanged.emit(self.begin, self.end)
+
+    def checkData(self):
+        if self._data:
+            return True
+        return False
+
+    def loadNext(self):
+        self.begin += self.PER_PAGE
+        self.end += self.PER_PAGE
+        if self.begin > len(self._data) - self.PER_PAGE:
+            if self.end >= len(self._data) and self.begin < len(self._data):
+                self.begin = self.begin
+            elif self.end >= len(self._data) and self.begin >= len(self._data):
+                self.begin -= self.PER_PAGE
+            else:
+                self.begin = len(self._data) - self.PER_PAGE
+        if self.end > len(self._data):
+            self.end = len(self._data)
+
+        self.beginResetModel()
+        self._displayed_data = self._data[self.begin:self.end]
+        self.endResetModel()
+
+        self.indexesChanged.emit(self.begin, self.end)
+
+    def loadPrevious(self):
+        self.end = self.begin
+        if self.end < self.PER_PAGE:
+            if len(self._data) >= self.PER_PAGE:
+                self.end = self.PER_PAGE
+            else:
+                self.end = len(self._data)
+
+        self.begin -= self.PER_PAGE
+        if self.begin < 0:
+            self.begin = 0
+
+        self.beginResetModel()
+        self._displayed_data = self._data[self.begin:self.end]
+        self.endResetModel()
+
+        self.indexesChanged.emit(self.begin, self.end)
--- models/contacts.py
+++ models/contacts.py
@@ -1,117 +1,43 @@
-from PyQt5.QtCore import QAbstractListModel, Qt, pyqtSignal
+from models.base import BaseListModel
+
+from PyQt5.QtCore import Qt
 
 from options import Options
 
 
-class ContactsListModel(QAbstractListModel):
+class ThreadsListModel(BaseListModel):
 
     PER_PAGE = Options.app_options['contacts_per_page']
-    indexesChanged = pyqtSignal(int, int)
 
     def __init__(self, data=None, parent=None):
-        super().__init__(parent)
+        super().__init__(data, parent)
 
         Options.optionsChanged.connect(self.change_per_page)
-
-        self._data = data if data else []
-        self.begin = 0
-        self.end = self.PER_PAGE
-        self._displayed_data = self._data[self.begin:self.end]
-
-    def rowCount(self, parent=None):
-        return len(self._displayed_data)
-
-    def displayedIndex(self):
-        # this method might be useful for pagination and it would
-        # be more useful outisde of this model.
-        return self.begin, self.end
 
     def data(self, index, role=None):
         if role == Qt.DisplayRole:
             return self._displayed_data[index.row()]
 
         elif role == Qt.DecorationRole:
-            # return pixmap of first letter of Person's name
+            # pix = QPixmap(self.filepath)
+            # painter = QPainter(pix)
+            # font = QFont('Arial')
+            # font.setPixelSize(12)
+            # painter.setFont(font)
+            #
+            # snippet = self._displayed_data[index.row()].snippet
+            # if snippet:
+            #     painter.drawText(QPoint(12, 20), snippet[0].upper())
+            # else:
+            #     painter.drawText(QPoint(12, 20), ' ')
+            # return pix
             pass
 
         elif role == Qt.ToolTipRole:
             pass
 
-
-    def flags(self, index):
-        return Qt.ItemIsEnabled | Qt.ItemIsSelectable
-
-    def insertRows(self, position, rows, parent=None):
-        pass
-
-    def removeRows(self, position, rows, parent=None):
-        pass
-
-    def createData(self, data):
-        # Use only if no widgets are connected to this model,
-        # if you have connected widgets use "addData" instead.
-        self._data = data
-        self._displayed_data = self._data[self.begin:self.end]
-
-    def addData(self, data):
-        # Adds data to already existing data, so model reset is required.
-        self.beginResetModel()
-        self._data = data + self._data
-        self._displayed_data = self._data[self.begin:self.end]
-        self.endResetModel()
-        self.indexesChanged.emit(self.begin, self.end)
-
-    def replaceData(self, data):
-        self.beginResetModel()
-        self._data = data
-        self._displayed_data = self._data[self.begin:self.end]
-        self.endResetModel()
-        self.indexesChanged.emit(self.begin, self.end)
-
-    def checkData(self):
-        if self._data:
-            return True
-        return False
-
-    def loadNext(self):
-        self.begin += self.PER_PAGE
-        self.end += self.PER_PAGE
-        if self.begin > len(self._data) - self.PER_PAGE:
-            if self.end >= len(self._data) and self.begin < len(self._data):
-                self.begin = self.begin
-            elif self.end >= len(self._data) and self.begin >= len(self._data):
-                self.begin -= self.PER_PAGE
-            else:
-                self.begin = len(self._data) - self.PER_PAGE
-        if self.end > len(self._data):
-            self.end = len(self._data)
-
-        self.beginResetModel()
-        self._displayed_data = self._data[self.begin:self.end]
-        self.endResetModel()
-
-        self.indexesChanged.emit(self.begin, self.end)
-
-    def loadPrevious(self):
-        self.end = self.begin
-        if self.end < self.PER_PAGE:
-            if len(self._data) >= self.PER_PAGE:
-                self.end = self.PER_PAGE
-            else:
-                self.end = len(self._data)
-
-        self.begin -= self.PER_PAGE
-        if self.begin < 0:
-            self.begin = 0
-
-        self.beginResetModel()
-        self._displayed_data = self._data[self.begin:self.end]
-        self.endResetModel()
-
-        self.indexesChanged.emit(self.begin, self.end)
-
     def change_per_page(self):
-        self.PER_PAGE = Options.app_options['contacts_per_page']
+        self.PER_PAGE = Options.app_options['threads_per_page']
 
         self.begin = 0
         self.end = self.PER_PAGE
