df2bf26c324858f0b42dfaa68b520006a47e5705
aleksaa01 <some12curious@gmail.com>
2018-12-21 15:36:39 +0100

    Initialize attributes first and then return if contact_dict is None. contact_dict is specified only when loading from the api.

--- googleapis/people/contact_objects.py
+++ googleapis/people/contact_objects.py
@@ -1,14 +1,18 @@
 class ContactObject(object):
 
-    def __init__(self, contact_dict):
+    def __init__(self, contact_dict=None):
+        self.name = ''
+        self.email = ''
+
+        if contact_dict is None:
+            return
+
         self.resource_name = contact_dict['resourceName']
         self.etag = contact_dict['etag']
 
         names = contact_dict.get('names', [])
         emails = contact_dict.get('emailAddresses', [])
 
-        self.name = ''
-        self.email = ''
         if names:
             self.name = names[0]['displayName']
         if emails:
