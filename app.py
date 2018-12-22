24fdec87b9e491030bf72af2154714658e7aef01
aleksaa01 <some12curious@gmail.com>
2018-12-22 17:23:27 +0100

    Merge branch 'master' of /home/asynclife/Documents/PythonProjects/qgmailer with conflicts.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sys
 from dispatcher import Dispatcher
 from views.main_view import MainView
 from options import Options
-
+from PyQt5.QtGui import QFontDatabase
 
 def change_app_stylesheet(app):
     app.setStyleSheet(Options.extract_theme())
@@ -12,6 +12,7 @@ def change_app_stylesheet(app):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     app.setStyleSheet(Options.extract_theme())
+    QFontDatabase.addApplicationFont(':/fonts/NotoSans/NotoSans-Regular.ttf')
     Options.optionsChanged.connect(lambda: change_app_stylesheet(app))
     dispatcher = Dispatcher()
     window = MainView(dispatcher)
