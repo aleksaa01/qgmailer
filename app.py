5bf30698f5c99c6faacf86df079f181d76cbf262
aleksaa01 <some12curious@gmail.com>
2018-12-25 17:20:19 +0100

    App now updates both the theme and default font-size.

--- app.py
+++ app.py
@@ -9,24 +9,37 @@ from options import Options
 from PySide2.QtGui import QFont
 
 
-def change_app_stylesheet(app):
+SYSTEM_OS = platform.system()
+
+
+def change_appearance(app):
+    # change theme at the end so that changes can take effect.
+    change_default_font(app)
     app.setStyleSheet(Options.extract_theme())
+
+
+def change_default_font(app):
+    # avoid weird characters in Linux by setting "DejaVu Sans"
+    # as a default font and restricting font merging.
+    if SYSTEM_OS == 'Windows':
+        default_font = QFont()
+        default_font.setPixelSize(Options.app_options['font_size'])
+        app.setFont(default_font)
+    elif SYSTEM_OS == 'Linux':
+        default_font = QFont('DejaVu Sans')
+        default_font.setPixelSize(Options.app_options['font_size'])
+        QFont.setStyleStrategy(default_font, QFont.NoFontMerging)
+        app.setFont(default_font)
+
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    change_default_font(app)
     app.setStyleSheet(Options.extract_theme())
 
-    system_os = platform.system()
-    # avoid weird characters in Linux by setting "DejaVu Sans"
-    # as a default font and restricting font merging.
-    if system_os == 'Linux':
-        default_font = QFont('DejaVu Sans')
-        default_font.setPixelSize(13)
-        QFont.setStyleStrategy(default_font, QFont.NoFontMerging)
-        app.setFont(default_font)
+    Options.optionsChanged.connect(lambda: change_appearance(app))
 
-    Options.optionsChanged.connect(lambda: change_app_stylesheet(app))
     dispatcher = Dispatcher()
     window = MainView(dispatcher)
     window.show()
