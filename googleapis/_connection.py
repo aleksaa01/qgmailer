6e4834641c5b035fad897457de02d74893efa049
aleksaa01 <some12curious@gmail.com>
2019-01-20 13:27:54 +0100

    Migrating from authentication with oauth2client(long deprecated) to authentication with google_auth.

--- googleapis/_connection.py
+++ googleapis/_connection.py
@@ -1,7 +1,9 @@
-from httplib2 import Http
-from oauth2client import tools, file, client
 from googleapiclient import discovery
-from oauth2client.clientsecrets import InvalidClientSecretsError
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from oauthlib.oauth2.rfc6749.errors import InvalidClientError
+
+import json
 
 
 class ConnectionBase(object):
@@ -14,16 +16,45 @@ class ConnectionBase(object):
 
     def __init__(self):
         self._res_list = []
+        self.user_config = None
+        self.credentials = None
+        try:
+            self.authorize_user()
+            # if user is successfully authorized, no need to call run_flow so just return
+            return
+        except (ValueError, FileNotFoundError):
+            # user config file doesn't exist or is invalid, ignore error and call run_flow
+            pass
+        try:
+            self.run_flow()
+        except (FileNotFoundError, InvalidClientError):
+            self.client_secret_error()
 
-        self.store = file.Storage(self.storage)
-        self.credentials = self.store.get()
+    def authorize_user(self):
+        if self.user_config is None:
+            with open(self.storage, 'r') as file:
+                self.user_config = json.load(file)
 
-        if not self.credentials or self.credentials.invalid:
-            try:
-                flow = client.flow_from_clientsecrets(self.client_secret, self.scopes)
-                self.credentials = tools.run_flow(flow, self.store)
-            except InvalidClientSecretsError:
-                self.client_secret_error()
+        self.credentials = Credentials.from_authorized_user_info(self.user_config, self.scopes)
+
+    def run_flow(self):
+        with open(self.client_secret, 'r') as file:
+            client_config = json.load(file)
+
+        flow = InstalledAppFlow.from_client_config(client_config, self.scopes)
+        self.credentials = InstalledAppFlow.run_local_server(flow)
+
+        # save obtained credentials for faster authorization next time
+        self.save_credentials()
+
+    def save_credentials(self):
+        user_config = {}
+        with open(self.storage, 'w') as file:
+            user_config['refresh_token'] = self.credentials.refresh_token
+            user_config['client_id'] = self.credentials.client_id
+            user_config['client_secret'] = self.credentials.client_secret
+            self.user_config = user_config
+            json.dump(user_config, file)
 
     def client_secret_error(self):
         raise NotImplemented('Classes that inherit from BaseConnection have to implement this method.')
@@ -36,6 +67,5 @@ class ConnectionBase(object):
 
     def _establish_new_connection(self):
         """ Returns a Resource object for interacting with an API."""
-        http = self.credentials.authorize(Http())
-        resource = discovery.build(self.api_name, self.api_version, http=http)
+        resource = discovery.build(self.api_name, self.api_version, credentials=self.credentials)
         return resource
