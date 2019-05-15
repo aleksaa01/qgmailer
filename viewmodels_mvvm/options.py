6e8e2d096e82219e150b26f7c71b6b126298630e
Aleksa Avramovic <some12curious@gmail.com>
2019-05-15 10:53:14 +0200

    Use observer pattern instead of signals/slots, because there is no reason to make viewmodels inherit from QObjects.

--- viewmodels_mvvm/options.py
+++ viewmodels_mvvm/options.py
@@ -3,20 +3,28 @@ from PyQt5.QtCore import pyqtSignal
 from options import Options
 from viewmodels_mvvm._singleton import SingletonViewModel
 
+import weakref
+
 
 class OptionsViewModel(object, metaclass=SingletonViewModel):
-
-    optionsChanged = pyqtSignal()
 
     def __init__(self):
         self._options = Options
         self._current_theme = None
+
+        self._callbacks = []
+
+        self._options.optionsChanged.connect(self.notify)
 
     def run(self):
         return
 
     def change_option(self, name, value, save=True):
         self._options.change_option(name, value, save)
+
+    def replace_options(self, new_options):
+        self._options.app_options = new_options
+        self._options.save()
 
     @property
     def current_theme(self):
@@ -27,8 +35,18 @@ class OptionsViewModel(object, metaclass=SingletonViewModel):
         self._current_theme = self._options.extract_theme(name)
         return self._current_theme
 
+    def extract_theme(self):
+        return self._options.extract_theme(self.current_theme)
+
     def all_options(self):
         return self._options.all_options
 
     def current_options(self):
         return self._options.app_options
+
+    def register(self, callback):
+        self._callbacks.append(callback)
+
+    def notify(self):
+        for callback in self._callbacks:
+            callback()
