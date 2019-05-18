a27f0d6152e72560bf246646bd0d61718b804d85
Aleksa Avramovic <some12curious@gmail.com>
2019-05-18 13:23:01 +0200

    Added few new methods.

--- viewmodels_mvvm/emails.py
+++ viewmodels_mvvm/emails.py
@@ -1,15 +1,29 @@
 from viewmodels_mvvm._singleton import SingletonViewModel
 from fetchers_mvvm.messages import MessageContentFetcher
+from googleapis.gmail.gparser import extract_body
 
 
 class MessageContentViewModel(object, metaclass=SingletonViewModel):
 
     def __init__(self):
-        self._srv = None
-
+        self._fetcher = MessageContentFetcher()
+        self._on_fetched_list = []
+        self._fetcher.fetched.connect(lambda data: self.notify(self._on_fetched_list, data))
 
     def assign_service(self, service):
-        self._srv = service
+        self._fetcher.set_service(service)
 
     def fetch_data(self, message_id):
-        pass
+        self._fetcher.set_message_id(message_id)
+        self._fetcher.start()
+
+    def on_fetched(self, callback):
+        self._on_fetched_list.append(callback)
+
+    def notify(self, callbacks, data):
+        data = self._decode_data(data)
+        for callback in callbacks:
+            callback(data)
+
+    def _decode_data(self, data):
+        return extract_body(data)
