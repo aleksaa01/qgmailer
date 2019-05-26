f3cb06f1e386766a63948553ceecbd787a63c47c
Aleksa Avramovic <some12curious@gmail.com>
2019-05-26 22:06:37 +0200

    Added new view model for email sending.

---
+++ viewmodels_mvvm/send_email.py
@@ -0,0 +1,18 @@
+from googleapis.gmail.connection import GConnection
+from googleapis.gmail.resources import ResourcePool
+from googleapis.gmail.send import EmailSender
+
+
+class SendEmailViewModel(object):
+
+    def __init__(self):
+        # At this point ResourcePool is already initialized
+        # pass None as Resource is already initialized this point
+        self.resource_pool = ResourcePool(GConnection())
+        self.email_sender = None
+
+    def run(self):
+        self.email_sender = EmailSender(self.resource_pool.get())
+
+    def send_email(self, to, subject, text):
+        self.email_sender.send_email(to, subject, text)
