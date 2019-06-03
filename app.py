4ae2461a2da2e896b9d173467fd108ab1a727a3e
Aleksa Avramovic <some12curious@gmail.com>
2019-06-03 15:24:49 +0200

    Remove completed TODOs.

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ import sys
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 
 
-# TODO: Fix layout of Options page.
 # TODO: Notify user if message was sent successfully or not.
-# FIXME: Find the solution to single-releasable-shared-resource problem/bug.
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
