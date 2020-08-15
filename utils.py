5f0299b60f13d1d41f62b0f6890a3272fa04b29d
Aleksa Avramovic <some12curious@gmail.com>
2020-08-15 15:52:03 +0200

    Move APIEvent to temporary location outside of app.py for proper serialization/deserialization.

--- utils.py
+++ utils.py
@@ -5,3 +5,11 @@ class Singleton(type):
         if cls._instance is None:
             cls._instance = super().__call__(*args, **kwargs)
         return cls._instance
+
+
+class APIEvent(object):
+    def __init__(self, event_id, type, category='', value=0):
+        self.event_id = event_id
+        self.type = type
+        self.category = category
+        self.value = value
