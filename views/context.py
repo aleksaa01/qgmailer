6b9dbbbc61139ce68811360c5e8e9b7e92653b38
Aleksa Avramovic <some12curious@gmail.com>
2020-12-22 22:18:01 +0100

    Fixed context class for context menus.

--- views/context.py
+++ views/context.py
@@ -1,16 +1,20 @@
 from PyQt5.QtWidgets import QMenu
 from PyQt5.QtGui import QIcon, QPixmap
 
+from channels.signal_channels import SignalChannel
+
 
 class ContactContext(object):
+
+    on_removed = SignalChannel()
+
     def __init__(self):
-        self._on_removed = lambda: None
         self.qmenu = QMenu()
         icon = QIcon(QPixmap(':/images/remove_icon.png'))
-        self.qmenu.addAction(icon, 'Remove', self._on_removed)
+        self.qmenu.addAction(icon, 'Remove', self.action_remove)
 
-    def on_removed(self, callback):
-        self._on_removed = callback
+    def action_remove(self):
+        self.on_removed.emit()
 
     def show(self, position):
         self.qmenu.exec_(position)
