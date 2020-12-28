ae12b9fdba243f61f0144e04446ccee42c3d3174
Aleksa Avramović <33311811+aleksaa01@users.noreply.github.com>
2020-12-28 13:17:10 +0100

    Update APIEvent class.

--- services/event.py
+++ services/event.py
@@ -2,7 +2,8 @@ IPC_SHUTDOWN = 1
 
 
 class APIEvent(object):
-    def __init__(self, event_id, category='', value=None):
+    def __init__(self, event_id, event_channel, topic, **payload):
         self.event_id = event_id
-        self.category = category
-        self.value = value
+        self.event_channel = event_channel
+        self.topic = topic
+        self.payload = payload
