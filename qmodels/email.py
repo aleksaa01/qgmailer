230df096b9592e170c827bc08a74237eb3161762
Aleksa Avramović <33311811+aleksaa01@users.noreply.github.com>
2020-12-28 13:11:05 +0100

    Update email model.

--- qmodels/email.py
+++ qmodels/email.py
@@ -3,7 +3,7 @@ from PyQt5.QtCore import Qt
 from qmodels.base import BaseListModel
 from qmodels.options import options
 from channels.event_channels import EmailEventChannel, OptionEventChannel
-from datetime import datetime
+from channels.sync import SyncHelper
 
 
 class EmailModel(BaseListModel):
@@ -20,7 +20,9 @@ class EmailModel(BaseListModel):
 
         # Get first page
         self.fetching = True
-        EmailEventChannel.publish('page_request', {'category': self.category})
+        EmailEventChannel.publish('page_request', category=self.category)
+
+        self.sync_helper = SyncHelper()
 
     def data(self, index, role=Qt.DisplayRole):
         if role == Qt.DisplayRole:
@@ -37,28 +39,36 @@ class EmailModel(BaseListModel):
             return None, None
         return self.begin, self.end
 
-    def add_new_page(self, message):
-        if message.get('category') != self.category:
+    def add_new_page(self, category, emails, error=''):
+        if category != self.category:
             return
+
+        if error:
+            # TODO: Handle this error somehow.
+            print("Page request failed... Error: ", error)
+            raise Exception()
         self.fetching = False
+
+        for email in emails:
+            email['ulid'] = self.sync_helper.new_ulid()
+
         if self.end == 0:
             # Model is empty, just add data, don't load next page.
-            self.add_data(message.get('value'))
+            self.add_data(emails)
         else:
-            data = message.get('value')
-            self.add_data(message.get('value'), notify=False)
+            self.add_data(emails, notify=False)
             self.load_next()
 
     def emit_email_id(self, idx):
-        EmailEventChannel.publish('email_request', {'category': 'email_content', 'value': self._displayed_data[idx].get('id')})
+        EmailEventChannel.publish('email_request', email_id=self._displayed_data[idx].get('id'))
 
-    def change_page_length(self, message):
-        self.set_page_length(message.get('value'))
+    def change_page_length(self, page_length):
+        self.set_page_length(page_length)
 
     def load_next_page(self):
         if self.end == len(self._data):
             self.fetching = True
-            EmailEventChannel.publish('page_request', {'category': self.category})
+            EmailEventChannel.publish('page_request', category=self.category)
             return
 
         self.load_next()
