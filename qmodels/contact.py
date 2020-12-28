5f5ba8380ad9d06eb9fa493f6293629f3e84c1ac
Aleksa Avramović <33311811+aleksaa01@users.noreply.github.com>
2020-12-28 13:01:37 +0100

    Update contact model.

--- qmodels/contact.py
+++ qmodels/contact.py
@@ -6,13 +6,17 @@ from channels.event_channels import ContactEventChannel, OptionEventChannel
 from channels.sync import SyncHelper
 
 
+# TODO: Another issue that might come up: How about removing a contact
+#   in the middle of "page fetching process". Check if you have to adjust
+#   begin and end attributes. Page processing will be blocked during this
+#   so you don't have to worry about those kind of inconsistencies.
+
 class ContactModel(BaseListModel):
 
-    def __init__(self, category, data=None):
+    def __init__(self, data=None):
         super().__init__(data)
 
         self.page_length = options.contacts_per_page
-        self.category = category
         self.fetching = False
 
         ContactEventChannel.subscribe('page_response', self.add_new_page)
@@ -22,7 +26,7 @@ class ContactModel(BaseListModel):
 
         # Get first page
         self.fetching = True
-        ContactEventChannel.publish('page_request', {'category': self.category})
+        ContactEventChannel.publish('page_request')
 
         self.sync_helper = SyncHelper()
 
@@ -41,26 +45,26 @@ class ContactModel(BaseListModel):
             return None, None
         return self.begin, self.end
 
-    def add_new_page(self, message):
-        if message.get('category') != self.category:
-            return
+    def add_new_page(self, contacts, error=''):
+        if error:
+            # TODO: Handle this error somehow.
+            print("Page request failed... Error: ", error)
+            raise Exception()
         self.fetching = False
 
-        data = message.get('value')
         # apply unique local IDs
-        for field in data:
+        for field in contacts:
             field['ulid'] = self.sync_helper.new_ulid()
 
         if self.end == 0:
             # Model is empty, just add data, don't load next page.
-            self.add_data(data)
+            self.add_data(contacts)
         else:
-            self.add_data(data, notify=False)
+            self.add_data(contacts, notify=False)
             self.load_next()
 
     def emit_email(self, idx):
-        ContactEventChannel.publish('contact_picked',
-                                  {'category': self.category, 'value': self._displayed_data[idx].get('email')})
+        ContactEventChannel.publish('contact_picked', email=self._displayed_data[idx].get('email'))
 
     def change_page_length(self, message):
         self.set_page_length(message.get('value'))
@@ -68,7 +72,7 @@ class ContactModel(BaseListModel):
     def load_next_page(self):
         if self.end == len(self._data):
             self.fetching = True
-            ContactEventChannel.publish('page_request', {'category': self.category})
+            ContactEventChannel.publish('page_request')
             return
 
         self.load_next()
@@ -80,10 +84,10 @@ class ContactModel(BaseListModel):
         print(f">>> Removing contact at index {idx}:", self._displayed_data[idx].get('email'))
         contact = self._displayed_data[idx]
         topic = 'remove_contact'
-        message = {'category': 'remove_contact', 'value': contact.get('resourceName')}
+        payload = {'resourceName': contact.get('resourceName')}
         # if event list is empty you can send the request, otherwise you have to wait
         # for the response to be processed until you can send another one.
-        self.sync_helper.push_event(ContactEventChannel, topic, message, contact)
+        self.sync_helper.push_event(ContactEventChannel, topic, payload, contact)
 
         self._data.pop(self.begin + idx)
         self.end = self.begin + min(self.page_length, len(self._data))
@@ -91,24 +95,26 @@ class ContactModel(BaseListModel):
         self._displayed_data = self._data[self.begin:self.end]
         self.endResetModel()
 
-    def handle_contact_removed(self, message):
-        if message.get('value').get('error'):
-            print("Failed to remove contact...")
+    def handle_contact_removed(self, error=''):
+        if error:
+            # TODO: Handle this error. Maybe, drop all data and then resync everything again.
             # Maybe, drop all data and then sync again.
-            return
+            print("Failed to remove contact...")
+            raise Exception()
 
         # we got a successful response back, now remove the event
         self.sync_helper.pull_event()
+        print("Contact completely removed.")
         # Now send next event if there's any left in the queue
         self.sync_helper.push_next_event()
 
     def add_contact(self, name, email):
         print(f"Adding new contact(name, email): {name}, {email}")
         topic = 'add_contact'
-        message = {'category': 'add_contact', 'value': {'name': name, 'email': email}}
+        payload = {'name': name, 'email': email}
         contact = {'name': name, 'email': email, 'ulid': self.sync_helper.new_ulid()}
 
-        self.sync_helper.push_event(ContactEventChannel, topic, message, contact)
+        self.sync_helper.push_event(ContactEventChannel, topic, payload, contact)
 
         self._data.insert(0, contact)
         self.end = self.begin + min(self.page_length, len(self._data))
@@ -116,34 +122,33 @@ class ContactModel(BaseListModel):
         self._displayed_data = self._data[self.begin:self.end]
         self.endResetModel()
 
-    def handle_contact_added(self, message):
-        api_contact = message.get('value')
-        if api_contact.get('error'):
-            print("Failed to add contact...")
-            # Maybe, drop all data and then sync again.
-            return
+    def handle_contact_added(self, name, email, resourceName, etag, error=''):
+        if error:
+            # TODO: Handle this error. Maybe, drop all data and then resync everything again.
+            print("Couldn't add new contact. Error: ", error)
+            raise Exception()
 
-        _, _, message, contact = self.sync_helper.pull_event()
+        _, topic, payload, contact = self.sync_helper.pull_event()
         ulid = contact.get('ulid')
         found = False
         for idx, con in enumerate(self._data):
             if con.get('ulid') == ulid:
-                # TODO: If contact was modified, you will also have to update the value of the message
-                if message.get('category') != 'add_contact':
+                # TODO: If contact was modified, you will also have to update the resourceName of the payload
+                if topic != 'add_contact':
                     # TODO: Update this when you add contact_modified topic to ContactEventChannel
                     pass
                 # Contact might've been updated so just add resourceName and etag
-                self._data[idx]['resourceName'] = api_contact.get('resourceName')
-                self._data[idx]['etag'] = api_contact.get('etag')
+                self._data[idx]['resourceName'] = resourceName
+                self._data[idx]['etag'] = etag
                 found = True
                 break
         if found is False:
-            # Contact was deleted, just update value of the event message in event queue
+            # Contact was deleted, just update the resourceName of event payload in the event queue
             found = False
             for event in self.sync_helper.events():
-                _, _, message, contact = event
+                _, _, payload, contact = event
                 if contact.get('ulid') == ulid:
-                    message['value'] = api_contact['resourceName']
+                    payload['resourceName'] = resourceName
                     found = True
                     break
             # If found is False something really went wrong.
