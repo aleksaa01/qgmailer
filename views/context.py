7601070d545d5b06c2e4cb7a99e7dc2f2f5689b0
Aleksa Avramović <33311811+aleksaa01@users.noreply.github.com>
2021-01-05 17:51:51 +0100

    Add two more contexts.
    Add inbox email context for inbox page and trash email context for trash page.

--- views/context.py
+++ views/context.py
@@ -18,3 +18,41 @@ class ContactContext(object):
 
     def show(self, position):
         self.qmenu.exec_(position)
+
+
+class InboxEmailContext(object):
+
+    on_trashed = SignalChannel()
+
+    def __init__(self):
+        self.qmenu = QMenu()
+        icon = QIcon(QPixmap(':/images/trash_icon.png'))
+        self.qmenu.addAction(icon, 'Move to Trash', self.action_trash)
+
+    def action_trash(self):
+        self.on_trashed.emit()
+
+    def show(self, position):
+        self.qmenu.exec_(position)
+
+
+class TrashEmailContext(object):
+
+    on_restored = SignalChannel()
+    on_deleted = SignalChannel()
+
+    def __init__(self):
+        self.qmenu = QMenu()
+        icon = QIcon(QPixmap('/images/trash_restore.png'))
+        self.qmenu.addAction(icon, 'Restore', self.action_restore)
+        icon = QIcon(QPixmap('/images/remove_icon.png'))
+        self.qmenu.addAction(icon, 'Delete forever', self.action_delete)
+
+    def action_restore(self):
+        self.on_restored.emit()
+
+    def action_delete(self):
+        self.on_deleted.emit()
+
+    def show(self, position):
+        self.qmenu.exec_(position)
