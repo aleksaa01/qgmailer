dccae58045c1cb0e6d40e8f0483db6340820796d
Aleksa Avramović <33311811+aleksaa01@users.noreply.github.com>
2021-01-05 18:27:38 +0100

    Update email.py
    Add methods to email model for moving emails to trash and response handling.

--- qmodels/email.py
+++ qmodels/email.py
@@ -75,3 +75,35 @@ class EmailModel(BaseListModel):
 
     def load_previous_page(self):
         self.load_previous()
+
+    def trash_email(self, idx):
+        print(f"Removing email at index {idx}:", self.displayed_data[idx].get('email'))
+        email = self.displayed_data[idx]
+        topic = 'remove_email'
+        payload = {'id': email.get('id'), 'from_ctg': self.category, 'to_ctg': ''}
+        self.sync_helper.push_event(EmailEventChannel, topic, payload, email)
+
+        self._data.pop(self.begin + idx)
+        self.end = min(self.begin + self.page_length, len(self._data))
+        self.beginResetModel()
+        self._displayed_data = self._data[self.begin:self.end]
+        self.endResetModel()
+
+    def handle_email_trashed(self, email, from_ctg, to_ctg, error=''):
+        if from_ctg != self.category and to_ctg != self.category:
+            return
+        # TODO: Formatting of the email should be done in api service in api call function.
+        if error:
+            # TODO: Handle this error. Maybe, drop all data and then resync everything again.
+            print("Failed to remove email...")
+            raise Exception()
+
+        if from_ctg == self.category:
+            # This is the inbox model, so now we can remove the event
+            self.sync_helper.pull_event()
+            print(f"Email completely removed(category: {self.category}.")
+            # Now send next event if there's any left in the queue
+            self.sync_helper.push_next_event()
+        elif to_ctg == self.category:
+            # This is the trash model, so now we add it to model data
+            self.add_data([email])
