c29bd283a3dc12879ba69932db1cbe1d89c59b01
Aleksa Avramović <33311811+aleksaa01@users.noreply.github.com>
2021-01-06 16:52:31 +0100

    Bugfix
    Next button was getting disabled even though there was more available data.

--- views/lists.py
+++ views/lists.py
@@ -71,12 +71,13 @@ class PageListView(QWidget):
     def update_indexes(self):
         old_idx1, old_idx2 = self.page_index.indexes()
         idx1, idx2 = self._model.current_index()
+        total_length = len(self._model)
         self.page_index.set_indexes(idx1, idx2)
         if idx1 == 0:
             self.page_index.enable_previous(False)
         else:
             self.page_index.enable_previous(True)
-        if idx1 == old_idx1 and idx2 == old_idx2:
+        if idx1 == old_idx1 and idx2 == old_idx2 and idx2 == total_length:
             self.page_index.enable_next(False)
         else:
             self.page_index.enable_next(True)
