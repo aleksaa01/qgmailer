aae4d4d9a3226d3405458be1f52eaa1a0ea48a03
Aleksa Avramović <33311811+aleksaa01@users.noreply.github.com>
2021-01-07 20:14:03 +0100

    Update shortcuts.py

--- views/shortcuts.py
+++ views/shortcuts.py
@@ -10,12 +10,14 @@ class Shortcuts(object):
     def __init__(self, parent):
         self.inbox_shortcut = QShortcut(QKeySequence(options.inbox_shortcut), parent)
         self.send_email_shortcut = QShortcut(QKeySequence(options.send_email_shortcut), parent)
+        self.sent_shortcut = QShortcut(QKeySequence(options.sent_shortcut), parent)
         self.contacts_shortcut = QShortcut(QKeySequence(options.contacts_shortcut), parent)
         self.trash_shortcut = QShortcut(QKeySequence(options.trash_shortcut), parent)
         self.options_shortcut = QShortcut(QKeySequence(options.options_shortcut), parent)
 
         self.inbox_shortcut.activated.connect(lambda: ShortcutEventChannel.publish('inbox_shortcut'))
         self.send_email_shortcut.activated.connect(lambda: ShortcutEventChannel.publish('send_email_shortcut'))
+        self.sent_shortcut.activated.connect(lambda: ShortcutEventChannel.publish('sent_shortcut'))
         self.contacts_shortcut.activated.connect(lambda: ShortcutEventChannel.publish('contacts_shortcut'))
         self.trash_shortcut.activated.connect(lambda: ShortcutEventChannel.publish('trash_shortcut'))
         self.options_shortcut.activated.connect(lambda: ShortcutEventChannel.publish('options_shortcut'))
@@ -25,6 +27,9 @@ class Shortcuts(object):
         )
         OptionEventChannel.subscribe(
             'send_email_shortcut', lambda send_email_shortcut: self.send_email_shortcut.setKey(QKeySequence(send_email_shortcut))
+        )
+        OptionEventChannel.subscribe(
+            'sent_shortcut', lambda sent_shortcut: self.sent_shortcut.setKey(QKeySequence(sent_shortcut))
         )
         OptionEventChannel.subscribe(
             'contacts_shortcut', lambda contacts_shortcut: self.contacts_shortcut.setKey(QKeySequence(contacts_shortcut))
