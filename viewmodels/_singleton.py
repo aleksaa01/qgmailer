fa2a34e78d3c070743fd8b117690087058103736
Aleksa Avramović <33311811+aleksaa01@users.noreply.github.com>
2021-01-10 14:42:18 +0100

    Remove viewmodels subdirectory

--- viewmodels/_singleton.py
+++
@@ -1,7 +0,0 @@
-class SingletonViewModel(type):
-    _instance = None
-
-    def __call__(cls, *args, **kwargs):
-        if cls._instance is None:
-            cls._instance = super().__call__(*args, **kwargs)
-        return cls._instance
