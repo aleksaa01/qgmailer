6328734d648f2016c925d92bf8b794ba4519b85e
Aleksa Avramović <33311811+aleksaa01@users.noreply.github.com>
2021-01-27 17:15:24 +0100

    multiprocessing.freeze_support() is needed when freezing the app in order to get rid of the spawning of multiple windows.

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from PyQt5.QtWidgets import QApplication
-# You have to import this before you can create QApplication
-from PyQt5.QtWebEngineWidgets import QWebEngineView
-# ----------------------------------------------------------
-
-import sys
-import multiprocessing
-import logging
-
-from views.app import AppView
-
-
 if __name__ == '__main__':
+    import multiprocessing
+    # This is required if you want to package your app with PyInstaller.
+    # This gets rid of the "multiple windows spawning" problem.
+    # Do this before you import anything else, you will avoid some performance issues.
+    multiprocessing.freeze_support()
+
+    from PyQt5.QtWidgets import QApplication
+    # You have to import this before you can create QApplication
+    from PyQt5.QtWebEngineWidgets import QWebEngineView
+    # ----------------------------------------------------------
+
+    import sys
+    import logging
+
+    from views.app import AppView
+
     app = QApplication(sys.argv)
 
     TESTING = True
