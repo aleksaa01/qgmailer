cd97b0d3a76794c1b3d692dd56fee1910a222292
Aleksa Avramović <33311811+aleksaa01@users.noreply.github.com>
2021-03-10 18:41:03 +0100

    Update send_email_page.py
    Fix icon of button for picking contacts.

--- views/send_email_page.py
+++ views/send_email_page.py
@@ -1,11 +1,14 @@
 from PyQt5.QtWidgets import QLineEdit, QTextEdit, QToolButton, QSizePolicy, QHBoxLayout, \
     QPushButton, QLabel, QVBoxLayout, QWidget
-from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtGui import QIcon, QPixmap, QPainter, QColor
 from PyQt5.QtCore import QSize, QTimer
 
 from googleapis.gmail.labels import LABEL_ID_SENT
-from channels.event_channels import EmailEventChannel, ContactEventChannel
+from channels.event_channels import EmailEventChannel, ContactEventChannel, \
+    OptionEventChannel
 from channels.signal_channels import SignalChannel
+from qmodels.options import options
+from views.buttons import AnimatedButton
 
 from email.mime.text import MIMEText
 from base64 import urlsafe_b64encode
@@ -45,6 +48,16 @@ class SendEmailPageView(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        pressed_hover = '#PickContacts:pressed:hover{border-radius: 16%%;}'
+        self.dark_style = pressed_hover + '#PickContacts{border: 0px; border-radius: 17%%; background-color: rgba(255, 255, 255, %s);}'
+        self.default_style = pressed_hover + '#PickContacts{border: 0px; border-radius: 17%%; background-color: rgba(0, 0, 0, %s);}'
+        if options.theme == 'dark':
+            color = QColor(255, 255, 255, 200)
+            self.current_style = self.dark_style
+        else:
+            self.current_style = self.default_style
+            color = QColor(0, 0, 0, 255)
+
         self.c = SendEmailPageController()
         self.c.on_email_sent.connect(self.handle_response)
         self.c.on_contact_picked.connect(self.add_recipient)
@@ -60,16 +73,17 @@ class SendEmailPageView(QWidget):
         self.subject_edit.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         
         self.message_text = QTextEdit(self)
-        self.pick_contacts_btn = QToolButton(self)
-        self.pick_contacts_btn.setFixedSize(30, 30)
-        self.pick_contacts_btn.setObjectName('findContactsBtn')
-        self.pick_contacts_btn.clicked.connect(self.pick_contact)
-        icon = QIcon(QPixmap(':/images/choose_contact_btn.png'))
-        self.pick_contacts_btn.setIcon(icon)
-        self.pick_contacts_btn.setIconSize(QSize(32, 32))
-        self.pick_contacts_btn.setStyleSheet(
-            '#findContactsBtn {background: transparent; border: none;} #findContactsBtn:hover {background: \"#b3b3b3\"; border-radius: 15px;}'
+        self.pick_contacts_btn = AnimatedButton(
+            lambda button, new_val: button.setStyleSheet(self.current_style % new_val),
+            anim_end=50, anim_duration=200
         )
+        self.pick_contacts_btn.setObjectName('PickContacts')
+        icon = self._prepare_pixmap(QPixmap(':/images/choose_contact_btn.png'), color)
+        self.pick_contacts_btn.setIcon(icon)
+        self.pick_contacts_btn.setIconSize(QSize(28, 28))
+        self.pick_contacts_btn.setFixedSize(QSize(35, 35))
+        self.pick_contacts_btn.setStyleSheet(self.current_style)
+        self.pick_contacts_btn.clicked.connect(self.pick_contact)
         
         self.send_email_btn = QPushButton('Send', self)
         # Setting minimum size will allow widget to grow when font size is increased.
@@ -96,6 +110,8 @@ class SendEmailPageView(QWidget):
         mlayout.addWidget(self.message_text)
         mlayout.addLayout(send_layout)
         self.setLayout(mlayout)
+
+        OptionEventChannel.subscribe('theme', self.update_icons)
 
     def pick_contact(self):
         self.c.pick_contact()
@@ -128,3 +144,21 @@ class SendEmailPageView(QWidget):
         self.send_email_message.show()
         
         QTimer.singleShot(5 * 1000, lambda: self.send_email_message.hide())
+
+    def update_icons(self, theme):
+        if theme == 'dark':
+            color = QColor(255, 255, 255, 200)
+            self.current_style = self.dark_style
+        else:
+            self.current_style = self.default_style
+            color = QColor(0, 0, 0, 255)
+
+        pix = self.pick_contacts_btn.icon().pixmap(QSize(28, 28))
+        self.pick_contacts_btn.setIcon(self._prepare_pixmap(pix, color))
+
+    def _prepare_pixmap(self, pixmap, qcolor):
+        painter = QPainter(pixmap)
+        painter.setCompositionMode(QPainter.CompositionMode_SourceAtop)
+        painter.fillRect(pixmap.rect(), qcolor)
+        painter.end()
+        return QIcon(pixmap)
