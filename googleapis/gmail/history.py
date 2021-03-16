4947c2d7f2751ab73bc7b622f8b2bfe3d5db9d64
Aleksa Avramović <33311811+aleksaa01@users.noreply.github.com>
2021-03-16 19:32:40 +0100

    Create history.py

---
+++ googleapis/gmail/history.py
@@ -0,0 +1,97 @@
+from googleapis.gmail.labels import LABEL_TO_LABEL_ID, LABEL_ID_TRASH, LABEL_ID_SENT, \
+    GMAIL_LABEL_TRASH, GMAIL_LABEL_SENT
+
+
+class HistoryRecord(object):
+    LABEL_TYPE_INBOX = 1
+    LABEL_TYPE_SENT = 2
+    ACTION_TRASH = 3
+    ACTION_RESTORE = 4
+    ACTION_ADD = 5
+    ACTION_DELETE = 6
+
+    def __init__(self, action=None, initial_label_id=None,
+                 message_id=None, history_id=None, label_type=None):
+        self.action = action
+        self.initial_label_id = initial_label_id
+        self.message_id = message_id
+        self.history_id = history_id
+        self.label_type = label_type
+
+        self.email = None
+
+    def set_record_info(self, action=None, initial_label_id=None,
+                        message_id=None, history_id=None, label_type=None):
+        self.action = action
+        self.initial_label_id = initial_label_id
+        self.message_id = message_id
+        self.history_id = history_id
+        self.label_type = label_type
+
+    def set_email(self, email):
+        self.email = email
+
+
+def parse_history_record(history_record, history_records_map):
+    """
+    :param history_record: Dictionary with keys: labelsAdded, labelsRemoved,
+    messagesAdded, messagesRemoved
+    :param history_records_map: Dictionary of <ID: HistoryRecord>
+    """
+    hid = history_record['id']
+    lbls_added = history_record.get('labelsAdded', [])
+    lbls_removed = history_record.get('labelsRemoved', [])
+    msgs_added = history_record.get('messagesAdded', [])
+    msgs_removed = history_record.get('messagesRemoved', [])
+
+    if lbls_added:
+        _parse_changes(lbls_added, HistoryRecord.ACTION_TRASH, hid, history_records_map)
+    if lbls_removed:
+        _parse_changes(lbls_removed, HistoryRecord.ACTION_RESTORE, hid, history_records_map)
+    if msgs_added:
+        _parse_changes(msgs_added, HistoryRecord.ACTION_ADD, hid, history_records_map)
+    if msgs_removed:
+        _parse_changes(msgs_removed, HistoryRecord.ACTION_DELETE, hid, history_records_map)
+
+
+def _parse_changes(changed_labels, action_type, history_id, history_records_map):
+    for msg in changed_labels:
+        message_labels = msg['message']['labelIds']
+        changed_labels = msg['labelIds']
+
+        if action_type == HistoryRecord.ACTION_TRASH or action_type == HistoryRecord.ACTION_RESTORE:
+            # Check if TRASH label was added to this email message and proceed, continue otherwise
+            if GMAIL_LABEL_TRASH not in changed_labels:
+                continue
+
+        # Figure out what type of email are we dealing with, this will make it easy for others to
+        # determine what metadata has to be fetched.
+        label_id = None
+        label_type = None
+        if GMAIL_LABEL_SENT in message_labels:
+            label_type = HistoryRecord.LABEL_TYPE_SENT
+            label_id = LABEL_ID_SENT
+        else:
+            for lbl in message_labels:
+                # Here we check if we can handle this label(for example DRAFT label is not handled)
+                label_id = LABEL_TO_LABEL_ID.get(lbl)
+                # Here we are figuring out initial label of this message
+                if label_id is not None and label_id != LABEL_ID_TRASH:
+                    label_type = HistoryRecord.LABEL_TYPE_INBOX
+                    # Break once you find a label_type, otherwise label_id might change the value.
+                    break
+        # If label_type is None, then there either is no real change, or we don't support that label.
+        if label_type is None:
+            continue
+
+        msg_id = msg['message']['id']
+
+        history_record = history_records_map.get(msg_id)
+        # Add new history record if it's not already present, modify record info otherwise.
+        if history_record is None:
+            history_record = HistoryRecord(action_type, label_id,
+                                           msg_id, history_id, label_type)
+            history_records_map[msg_id] = history_record
+        else:
+            history_record.set_record_info(action_type, label_id,
+                                           msg_id, history_id, label_type)
