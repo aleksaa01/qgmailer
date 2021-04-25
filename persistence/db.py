a75a413b6a4e2e9c0cdaa1cb4a83e7ea3a544426
Aleksa Avramović <33311811+aleksaa01@users.noreply.github.com>
2021-04-25 18:54:52 +0200

    Few fixes.

--- persistence/db.py
+++ persistence/db.py
@@ -11,23 +11,26 @@ def check_if_db_exists():
 
 
 def db_connect(path=DB_PATH):
-    return sqlite3.connect(path)
+    dbcon = sqlite3.connect(path)
+    # Enable foreign key support by default
+    enable_foreign_keys(dbcon.cursor())
+    return dbcon
 
 
 def enable_foreign_keys(cursor):
+    # You have to run this pragma in order to enable foreign key support.
     cursor.execute('PRAGMA foreign_keys = 1;')
 
 
 def db_setup():
-    if check_if_db_exists():
-        return
+    if not os.path.exists(os.path.dirname(DB_PATH)):
+        os.mkdir(os.path.dirname(DB_PATH))
 
     with open(DB_PATH, 'wb') as f:
         pass
 
     dbcon = db_connect()
     cur = dbcon.cursor()
-    enable_foreign_keys()
 
     # Create table for email messages.
     # Note that SQLite doesn't care about restricted length of columns.
@@ -36,19 +39,27 @@ def db_setup():
     # I don't know maximum length of the history-id yet.
     # Maximum email length(Email Path) is 256 characters. For example me@example.com:
     # Local-part(me) + @ + Domain(example.com) = Full Path(me@example.com)
+    # TODO: Consider maybe adding another text field to Message table,
+    #  which would be a comma separated list of all the Label-IDs
+    #  associated with that email message. This field would then have
+    #  to be deserialized -> updated -> serialized -> saved.
     cur.execute('''
     CREATE TABLE Message(
     pk INTEGER PRIMARY KEY,
     message_id VARCHAR(16) NOT NULL,
     history_id VARCHAR(32) NOT NULL,
-    to VARCHAR(256) NOT NULL,
-    from VARCHAR(256) NOT NULL,
+    field_to VARCHAR(256) NOT NULL,
+    field_from VARCHAR(256) NOT NULL,
     subject VARCHAR(256) DEFAULT "",
     snippet VARCHAR(256) DEFAULT "",
     date BIGINT NOT NULL
     );''')
     # Create table for keeping track of all Label IDs.
     # Individual label IDs should be represented by separate tables.
+    # TODO: Because we have messages_total column in here, it implicates
+    #  that we have to update it on every create/delete operation.
+    #  But we can only update it before the database updates. It might get
+    #  out of sync for short period of time, but that's fine.
     cur.execute('''
     CREATE TABLE LabelId (
     label_id VARCHAR(256) NOT NULL,
@@ -72,16 +83,18 @@ def db_setup():
     );''')
     cur.execute('CREATE UNIQUE INDEX emailindex ON Email(message_pk);')
 
+    return dbcon
+
 
 def create_label_table(cursor, label_id):
-    cursor.execute('''
-    CREATE TABLE ?(
+    cursor.execute(f'''
+    CREATE TABLE {label_id}(
     pk INTEGER PRIMARY KEY,
     message_pk INTEGER,
     CONSTRAINT fk_message
         FOREIGN KEY (message_pk)
         REFERENCES Message(pk)
         ON DELETE CASCADE
-    );''', (label_id,))
+    );''')
 
-    cursor.execute('CREATE UNIQUE INDEX ?index ON ?(message_pk);', (label_id,))
+    cursor.execute(f'CREATE UNIQUE INDEX {label_id}index ON {label_id}(message_pk);')
