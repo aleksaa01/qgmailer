57d648d282d70d7065a0c3cde25739fd512f5375
Aleksa Avramović <33311811+aleksaa01@users.noreply.github.com>
2021-10-09 21:16:17 +0200

    Update sidebar.py

--- views/sidebar.py
+++ views/sidebar.py
@@ -1,135 +1,338 @@
-from PyQt5.QtWidgets import QVBoxLayout, QFrame
-from PyQt5.QtCore import QSize, pyqtSignal
-from PyQt5.QtGui import QIcon, QPixmap, QColor, QPainter
+from PyQt5.QtWidgets import QFrame, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QSizePolicy, \
+    QWidget, QApplication, QScrollArea
+from PyQt5.QtCore import QSize, pyqtSignal, QRect, Qt, QTimer, QPropertyAnimation, pyqtProperty, \
+    QParallelAnimationGroup, QRectF
+from PyQt5.QtGui import QIcon, QPixmap, QColor, QPainter, QFontMetrics, QFont, QPainterPath
 
 from qmodels.options import options
-from channels.event_channels import OptionEventChannel
+from channels.event_channels import OptionEventChannel, EmailEventChannel
 from views.buttons import AnimatedCheckButton
+from googleapis.gmail.labels import *
 
 
+SCROLL_AREA_STYLESHEET = """
+    QAbstractScrollArea{
+        border: 0;
+        border-top: 1px solid #455364;
+        border-bottom: 1px solid #455364;
+        border-radius: 0;
+        padding: 0;
+    }
+    QScrollBar:vertical{
+        background-color: transparent;
+        width: 4px;
+        margin: 2px 0px 2px 0px;
+        border: 0;
+        border-radius: 1px;
+    }
+    QScrollBar::add-line:vertical {
+        border: none;
+        background: none;
+        width: 0;
+        height: 0;
+    }
+    QScrollBar::sub-line:vertical {
+        border: none;
+        background: none;
+        width: 0;
+        height: 0;
+    }
+"""
+
+
+# TODO: Add the ability to set item's icon color. You have to make sure that colors don't get changed
+#  on theme change.
 class Sidebar(QFrame):
-
     on_select = pyqtSignal(int)
+    on_item_pressed = pyqtSignal(int)
 
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self.dark_style = 'border: 0px; background-color: rgba(255, 255, 255, %s);'
-        self.default_style = 'border: 0px; background-color: rgba(0, 0, 0, %s);'
-        if options.theme == 'dark':
-            color = QColor(255, 255, 255, 200)
-            self.current_style = self.dark_style
+        self.current_item = None
+        self._next_widget_id = 0
+        # Instead of popping, put None in indecies of deleted items.
+        # This is fine if we don't intend to continuously add/remove lots of items.
+        # And it's cheap because it's a list of pointers.
+        self.items = []
+
+        self.layout = QVBoxLayout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.setSpacing(0)
+        self.setLayout(self.layout)
+        self.setFixedWidth(250)
+
+    def add_item(self, name, image_path):
+        item = SidebarItem(name, QPixmap(image_path))
+        self.layout.addWidget(item)
+        item_id = self._next_widget_id
+        self._next_widget_id += 1
+        self.items.append(item)
+        item.on_pressed.connect(lambda _: self.item_pressed(item_id))
+        return item_id
+
+    def add_group(self, group_name, max_height=400):
+        self.layout.addWidget(QLabel(f"<b>{group_name}</b>"), alignment=Qt.AlignHCenter)
+        group = QWidget()
+        group_layout = QVBoxLayout()
+        group_layout.setContentsMargins(0, 0, 0, 0)
+        group_layout.setSpacing(0)
+        group.setLayout(group_layout)
+        scroll_area = QScrollArea()
+        scroll_area.setStyleSheet(SCROLL_AREA_STYLESHEET)
+        scroll_area.setWidgetResizable(True)
+        scroll_area.setViewportMargins(0, 0, 0, 0)
+        scroll_area.setWidget(group)
+        scroll_area.setMaximumHeight(max_height)
+        self.layout.addWidget(scroll_area)
+
+        group_id = self._next_widget_id
+        self._next_widget_id += 1
+        self.items.append(group_layout)
+        return group_id
+
+    def add_item_to_group(self, group_id, item_name, item_image_path):
+        item = SidebarItem(item_name, QPixmap(item_image_path))
+        item_id = self._next_widget_id
+        self._next_widget_id += 1
+        self.items.append(item)
+        item.on_pressed.connect(lambda _: self.item_pressed(item_id))
+        group_layout = self.items[group_id]
+        group_layout.addWidget(item)
+        group_layout.update()
+
+        return item_id
+
+    def add_stretch(self):
+        self.layout.addStretch(1)
+
+    def remove_item(self, item_id, group_id=None):
+        item = self.items[item_id]
+        if self.current_item == item:
+            item.set_checked(False)
+            self.current_item = None
+
+        if group_id:
+            parent_layout = self.item[group_id]
         else:
-            self.current_style = self.default_style
-            color = QColor(0, 0, 0, 255)
+            parent_layout = self.layout
 
-        inbox_btn = AnimatedCheckButton(self.apply_stylesheet, anim_end=50, anim_duration=200)
-        icon = self._prepare_pixmap(QPixmap(':images/inbox_icon.png'), color)
-        inbox_btn.setIcon(icon)
-        inbox_btn.setIconSize(QSize(30, 30))
-        inbox_btn.setMinimumSize(QSize(45, 45))
-        inbox_btn.clicked.connect(lambda: self.emit_event(0))
+        item_idx = parent_layout.indexOf(item)
+        assert item_idx != -1
+        layout_item = parent_layout.takeAt(item_idx)
+        layout_item.widget().serParent(None)
+        del layout_item
 
-        send_email_btn = AnimatedCheckButton(self.apply_stylesheet, anim_end=50, anim_duration=200)
-        icon = self._prepare_pixmap(QPixmap(':/images/send_icon.png'), color)
-        send_email_btn.setIcon(icon)
-        send_email_btn.setIconSize(QSize(30, 30))
-        send_email_btn.setMinimumSize(QSize(45, 45))
-        send_email_btn.clicked.connect(lambda: self.emit_event(1))
+        self.items[item_id] = None
+        # TODO: Test if this is necessary at all.
+        self.update()
 
-        sent_btn = AnimatedCheckButton(self.apply_stylesheet, anim_end=50, anim_duration=200)
-        icon = self._prepare_pixmap(QPixmap(':/images/sent_icon.png'), color)
-        sent_btn.setIcon(icon)
-        sent_btn.setIconSize(QSize(30, 30))
-        sent_btn.setMinimumSize(QSize(45, 45))
-        sent_btn.clicked.connect(lambda: self.emit_event(2))
+    def change_item_name(self, item_id, new_name):
+        item = self.items[item_id]
+        item.text = new_name
+        # TODO: Is this even needed ?
+        self.update()
 
-        contacts_btn = AnimatedCheckButton(self.apply_stylesheet, anim_end=50, anim_duration=200)
-        icon = self._prepare_pixmap(QPixmap(':/images/contacts_icon2.png'), color)
-        contacts_btn.setIcon(icon)
-        contacts_btn.setIconSize(QSize(30, 30))
-        contacts_btn.setMinimumSize(QSize(45, 45))
-        contacts_btn.clicked.connect(lambda: self.emit_event(3))
+    def item_pressed(self, widget_id):
+        item = self.items[widget_id]
+        if self.current_item:
+            self.current_item.set_checked(False)
+        self.current_item = item
+        item.set_checked(True)
+        self.on_item_pressed.emit(widget_id)
 
-        trash_btn = AnimatedCheckButton(self.apply_stylesheet, anim_end=50, anim_duration=200)
-        icon = self._prepare_pixmap(QPixmap(':/images/trash_icon.png'), color)
-        trash_btn.setIcon(icon)
-        trash_btn.setIconSize(QSize(30, 30))
-        trash_btn.setMinimumSize(QSize(45, 45))
-        trash_btn.clicked.connect(lambda: self.emit_event(4))
-
-        options_btn = AnimatedCheckButton(self.apply_stylesheet, anim_end=50, anim_duration=200)
-        icon = self._prepare_pixmap(QPixmap(':/images/options_button.png'), color)
-        options_btn.setIcon(icon)
-        options_btn.setIconSize(QSize(30, 30))
-        options_btn.setMinimumSize(QSize(45, 45))
-        options_btn.clicked.connect(lambda: self.emit_event(5))
-
-        self.current_button = None
-        self.button_count = 6
-
-        main_layout = QVBoxLayout()
-        main_layout.addWidget(inbox_btn)
-        main_layout.addWidget(send_email_btn)
-        main_layout.addWidget(sent_btn)
-        main_layout.addWidget(contacts_btn)
-        main_layout.addWidget(trash_btn)
-        main_layout.addWidget(options_btn)
-        main_layout.addStretch(1)
-        main_layout.setSpacing(15)
-        main_layout.setContentsMargins(0, 0, 0, 0)
-        self.setLayout(main_layout)
-        OptionEventChannel.subscribe('theme', self.update_icons)
-
-    def emit_event(self, idx):
-        self.select(idx)
-        self.on_select.emit(idx)
-
-    def select(self, idx):
-        button = self.find_idx(idx)
-        if button is None:
-            if self.current_button:
-                self.current_button.set_checked(False)
-            self.current_button = None
+    def select_item(self, widget_id):
+        if widget_id is None:
+            if self.current_item:
+                self.current_item.set_checked(False)
+            self.current_item = None
         else:
-            if self.current_button:
-                self.current_button.set_checked(False)
-            self.current_button = button
-            button.set_checked(True)
-    
-    def find_idx(self, idx):
-        if idx >= self.button_count:
-            return None
-        cur_idx = -1
-        for widget in self.children():
-            if isinstance(widget, AnimatedCheckButton):
-                cur_idx += 1
-                if cur_idx == idx:
-                    return widget
+            item = self.items[widget_id]
+            if self.current_item:
+                self.current_item.set_checked(False)
+            self.current_item = item
+            item.set_checked(True)
 
-    def apply_stylesheet(self, button, new_val):
-        button.setStyleSheet(
-            self.current_style % new_val
-        )
 
-    def update_icons(self, theme):
-        if theme == 'dark':
-            color = QColor(255, 255, 255, 200)
-            self.current_style = self.dark_style
+class SidebarItem(QWidget):
+    on_pressed = pyqtSignal(bool)
+
+    def __init__(self, text, pixmap=None, parent=None):
+        super().__init__(parent)
+
+        rgb_value = 255 if options.theme == 'dark' else 0
+        self.theme = options.theme
+
+        # You are supposed to set size policy like this, and not to override sizePolicy()
+        # If vertical policy was preferred, then you would have to implement minimumSizeHint and
+        # maximumSize to limit how much the item can shrink and grow.
+        self.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.Fixed)
+        self.text = text
+        self.pixmap = pixmap
+        self._prepare_pixmap(QColor(rgb_value, rgb_value, rgb_value, 200))
+        self.spacing = 12
+        self.font = QApplication.font()
+        self.font.setBold(True)
+        self.fm = QFontMetrics(self.font)
+        self.mouse_over = False
+        self.checked = False
+        self.active = False
+        self.setMouseTracking(True)
+
+        self.background_color = QColor(rgb_value, rgb_value, rgb_value, 0)
+        self.background_animation = QPropertyAnimation(self, b'opacity')
+        self.background_animation.setStartValue(0)
+        self.background_animation.setEndValue(40)
+        self.background_animation.setDuration(300)
+
+        self.indicator_pos = 5
+        self.indicator_position_anim = QPropertyAnimation(self, b'indicator_position')
+        self.indicator_position_anim.setStartValue(5)
+        self.indicator_position_anim.setEndValue(-5)
+        self.indicator_position_anim.setDuration(300)
+
+        self.indicator_background = QColor(rgb_value, rgb_value, rgb_value, 0)
+        self.indicator_opacity_anim = QPropertyAnimation(self, b'indicator_opacity')
+        self.indicator_opacity_anim.setStartValue(0)
+        self.indicator_opacity_anim.setEndValue(140)
+        self.indicator_opacity_anim.setDuration(300)
+
+        self.all_animations = QParallelAnimationGroup()
+        self.all_animations.addAnimation(self.background_animation)
+        self.all_animations.addAnimation(self.indicator_position_anim)
+        self.all_animations.addAnimation(self.indicator_opacity_anim)
+
+        OptionEventChannel.subscribe('theme', self.handle_theme_change)
+
+    def mousePressEvent(self, event):
+        super().mousePressEvent(event)
+        self.on_pressed.emit(True)
+
+    def set_checked(self, checked):
+        self.checked = checked
+        self.check_active()
+
+    def check_active(self):
+        old_active = self.active
+        if self.checked:
+            self.active = True
         else:
-            self.current_style = self.default_style
-            color = QColor(0, 0, 0, 255)
+            self.active = self.mouse_over
 
-        for wgt in self.children():
-            if isinstance(wgt, AnimatedCheckButton):
-                pix = wgt.icon().pixmap(QSize(30, 30))
-                wgt.setIcon(self._prepare_pixmap(pix, color))
-                if wgt.isChecked():
-                    wgt.setStyleSheet(self.current_style % wgt.anim_end)
+        if old_active == self.active:
+            return
 
-    def _prepare_pixmap(self, pixmap, qcolor):
-        painter = QPainter(pixmap)
+        if self.active is True:
+            self.all_animations.setDirection(QParallelAnimationGroup.Forward)
+            self.all_animations.start()
+        else:
+            self.all_animations.setDirection(QParallelAnimationGroup.Backward)
+            self.all_animations.start()
+
+        # Calling update 10 times will result in only one update.
+        # So it's fine to do this even after starting the animations.
+        self.update()
+
+    def enterEvent(self, event):
+        self.mouse_over = True
+        self.check_active()
+
+    def leaveEvent(self, event):
+        self.mouse_over = False
+        self.check_active()
+
+    def sizeHint(self):
+        # sizeHint is used by layouts to get the recommended size of the widget.
+        return QSize(100, 40)
+
+    # def minimumSizeHint(self):
+    #     # minimumSizeHint is used by layouts to get the minimum size of the widget.
+    #     # It's ignored if minimumSize is also implemented
+    #     return self.sizeHint()
+
+    def paintEvent(self, paint_event):
+        super().paintEvent(paint_event)
+        widget_rect = self.rect()
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.fillRect(widget_rect, self.background_color)
+
+        if self.mouse_over:
+            indicator_rect = QRect(
+                widget_rect.width() + self.indicator_pos - 6, (widget_rect.height() - 6) // 2, 6, 9
+            )
+            triangle_path = QPainterPath(indicator_rect.topRight())
+            triangle_path.lineTo(indicator_rect.bottomRight())
+            mid_left = indicator_rect.bottomLeft()
+            mid_left.setY(mid_left.y() - indicator_rect.height() // 2)
+            triangle_path.lineTo(mid_left)
+            triangle_path.lineTo(indicator_rect.topRight())
+
+            painter.fillPath(triangle_path, self.indicator_background)
+
+        if self.pixmap:
+            pixmap_rect = QRect(self.spacing, (widget_rect.height() - 20) // 2, 20, 20)
+            painter.drawPixmap(pixmap_rect, self.pixmap, self.pixmap.rect())
+            # painter.drawRect(pixmap_rect)
+        else:
+            pixmap_rect = QRect(0, 0, 0, 0)
+
+        text_rect = QRect(pixmap_rect.right() + self.spacing, 0, 0, widget_rect.height())
+        text_rect.setWidth(widget_rect.width() - text_rect.left())
+        # painter.drawRect(text_rect)
+
+        text = self.fm.elidedText(self.text, Qt.ElideRight, text_rect.width())
+        painter.setFont(self.font)
+        painter.drawText(text_rect, Qt.AlignVCenter | Qt.AlignLeft, text)
+
+    def _prepare_pixmap(self, qcolor):
+        if self.pixmap is None:
+            return
+        painter = QPainter(self.pixmap)
         painter.setCompositionMode(QPainter.CompositionMode_SourceAtop)
-        painter.fillRect(pixmap.rect(), qcolor)
+        painter.fillRect(self.pixmap.rect(), qcolor)
         painter.end()
-        return QIcon(pixmap)
+
+    def _get_opacity(self):
+        return self.background_color.alpha()
+
+    def _set_opacity(self, new_value):
+        self.background_color.setAlpha(new_value)
+        self.update()
+
+    opacity = pyqtProperty('int', _get_opacity, _set_opacity)
+
+    def _get_indicator_position(self):
+        return self.indicator_pos
+
+    def _set_indicator_position(self, new_value):
+        self.indicator_pos = new_value
+        self.update()
+
+    indicator_position = pyqtProperty('int', _get_indicator_position, _set_indicator_position)
+
+    def _get_indicator_opacity(self):
+        return self.indicator_background.alpha()
+
+    def _set_indicator_opacity(self, new_value):
+        self.indicator_background.setAlpha(new_value)
+        self.update()
+
+    indicator_opacity = pyqtProperty('int', _get_indicator_opacity, _set_indicator_opacity)
+
+    def handle_theme_change(self, theme):
+        if theme == self.theme:
+            return
+
+        self.theme = theme
+        if theme == 'dark':
+            rgb_value = 255
+        else:
+            rgb_value = 0
+
+        self.background_color.setRgb(
+            rgb_value, rgb_value, rgb_value, self.background_color.alpha())
+        self.indicator_background.setRgb(
+            rgb_value, rgb_value, rgb_value, self.indicator_background.alpha())
+        self._prepare_pixmap(QColor(rgb_value, rgb_value, rgb_value, 200))
+        self.update()
