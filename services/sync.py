83b8a9a524622f7cfec6c61d7d07d4aebfd0b635
Aleksa Avramović <33311811+aleksaa01@users.noreply.github.com>
2021-10-09 21:09:57 +0200

    Update sync.py

--- services/sync.py
+++ services/sync.py
@@ -1,8 +1,12 @@
+from googleapis.gmail.labels import GMAIL_LABEL_TRASH
+from googleapis.gmail.history import HistoryRecord
 from channels.event_channels import EmailEventChannel
 from logs.loggers import default_logger, TESTING
 from services.errors import get_error_code
-from googleapis.gmail.labels import LABEL_ID_TRASH
-from googleapis.gmail.history import HistoryRecord
+from services.utils import email_message_to_dict
+
+import time
+
 
 LOG = default_logger()
 
@@ -13,6 +17,9 @@ class SyncHelper(object):
         # unique local ID counter, used internally for synchronization purposes
         # If you have to sync data with the external API, then add 'ulid' field 
         # to every item in self._data
+        # This is primarily used by the ContactModel because user can execute multiple actions in a
+        # very short amount of time, and thus you need something like this so you can handle any
+        # errors or successful operations in a sane manner.
         self.ulid_counter = 0
 
     def new_ulid(self):
@@ -45,6 +52,9 @@ class SyncHelper(object):
     def remove_event(self, idx):
         self._event_queue.pop(idx)
 
+    def __len__(self):
+        return len(self._event_queue)
+
 
 class Singleton(type):
     _instance = None
@@ -55,6 +65,7 @@ class Singleton(type):
         return cls._instance
 
 
+# TODO: Fix this one
 class EmailSynchronizer(metaclass=Singleton):
 
     def __init__(self):
@@ -65,99 +76,169 @@ class EmailSynchronizer(metaclass=Singleton):
     def register(self, model, label_id):
         self.registered_models[label_id] = model
 
-    def send_sync_request(self, max_results=10):
-        LOG.debug("In send_sync_request")
-        if self.last_history_id is None:
-            last_hid = 0
-            for model in self.registered_models.values():
-                first_item = model.get_item(0)
-                total_items = model.total_items()
-                if (total_items == -1 and first_item is None) or \
-                        (total_items > 0 and first_item is None):
-                    # Model didn't receive any data yet, so skip this sync request.
-                    LOG.info("Models data is not fetched yet, skipping sync request...")
-                    return
-                elif total_items == 0:
-                    # This model is empty, skip to the next one.
-                    continue
-                hid = int(first_item.get('historyId'))
-                last_hid = max(hid, last_hid)
-            self.last_history_id = last_hid
+    def send_sync_request(self):
+        LOG.warning("In send_sync_request")
+        EmailEventChannel.publish('short_sync')
 
-        LOG.info("Last history id: ", self.last_history_id)
-        EmailEventChannel.publish(
-            'short_sync', start_history_id=str(self.last_history_id), max_results=max_results)
-
-    def dispatch_updates(self, history_records, last_history_id, error=''):
+    def dispatch_updates(self, history_records, error=''):
         if error:
             parsed_error = get_error_code(error)
             if parsed_error == 404:
-                # TODO: Partial sync failed, do full sync.
+                # TODO: Partial sync failed, do full sync. Don't forget to wrap short_sync
+                #  api call in try-except block.
                 LOG.error(f"Partial sync failed. Error: {error}.")
                 return
         try:
-            self._dispatch_updates(history_records, last_history_id)
+            self._dispatch_updates(history_records)
         except Exception as err:
+            # TODO: Reset all models. And run full sync.
             LOG.error(f"Failed to dispatch history_records. Error: {err}")
             if TESTING is True:
                 raise err
 
-    def _dispatch_updates(self, history_records, last_history_id):
-        self.last_history_id = last_history_id
+    def _dispatch_updates(self, history_records):
         LOG.debug("Number of history records:", len(history_records))
-        LOG.info('-------- DISPATCHING HISTORY RECORDS --------')
-        for his_record in history_records:
-            model = self.registered_models.get(his_record.initial_label_id)
-            action = his_record.action
-            if action == HistoryRecord.ACTION_ADD:
-                email = his_record.email
-                if model.find_email(email.get('id'), email.get('internalDate')) == -1:
-                    model.insert_email(email)
-                    LOG.info(f"Inserted email with id={email.get('id')} into the "
-                             f"model with label_id={his_record.initial_label_id}")
+        LOG.warning('-------- DISPATCHING HISTORY RECORDS --------')
+        LOG.warning(f"History records: {history_records.values()}")
+        t1 = time.perf_counter()
+        for his in history_records.values():
+            LOG.warning("--- new history record ---")
+            # For every history record there are 3 possibilities:
+            # 1.) We were responsible for that
+            # 2.) Someone else is responsible for that
+            # 3.) Both? I think Gmail-API tries to squeeze multiple changes into one record.
+            m = his.message
+            parsed_email_message = email_message_to_dict(m)
+            if his.has_type(HistoryRecord.MESSAGE_DELETED):
+                LOG.warning("In HistoryRecord.MESSAGE_DELETED")
+                label_ids = m.label_ids.split(',')
+                LOG.warning(f"label_ids: {label_ids}")
+                if GMAIL_LABEL_TRASH in label_ids:
+                    LOG.warning("TRASH is present in label_ids")
+                    trash_model = self._get_model(GMAIL_LABEL_TRASH)
+                    idx = trash_model.find_email(m.message_id, m.internal_date)
+                    if idx != -1:
+                        trash_model.pop_email(m.message_id, idx)
                 else:
-                    LOG.info(f"Email with id={email.get('id')} is already present "
-                             f"in the model with label_id={his_record.initial_label_id}")
-            elif action == HistoryRecord.ACTION_DELETE:
-                if model.pop_email(his_record.message_id):
-                    LOG.info(f"Deleted email with id={his_record.message_id} from "
-                             f"the model with label_id={his_record.initial_label_id}")
+                    LOG.warning("TRASH is NOT present in label_ids")
+                    for lid in label_ids:
+                        model = self._get_model(lid)
+                        if not model:
+                            continue
+                        idx = model.find_email(m.message_id, m.internal_date)
+                        if idx != -1:
+                            model.pop_email(m.message_id, idx)
+            elif his.has_type(HistoryRecord.MESSAGE_ADDED):
+                LOG.warning("In HistoryRecord.MESSAGE_ADDED")
+                if his.has_type(HistoryRecord.LABELS_REMOVED):
+                    # Remove message from all 'removed labels'(can be thought of as 'old labels')
+                    LOG.warning(f"labels_removed: {his.labels_removed}")
+                    for lbl in his.labels_removed:
+                        model = self._get_model(lbl)
+                        if not model:
+                            continue
+                        idx = model.find_email(m.message_id, m.internal_date)
+                        if idx != -1:
+                            model.pop_email(m.message_id, idx)
+                label_ids = m.label_ids.split(',')
+                LOG.warning(f"label_ids: {label_ids}")
+                if GMAIL_LABEL_TRASH in label_ids:
+                    LOG.warning("TRASH is present in label_ids")
+                    # If message was added and it contains TRASH label, then we have to add it to
+                    # trash model.
+                    trash_model = self._get_model(GMAIL_LABEL_TRASH)
+                    if trash_model.find_email(m.message_id, m.internal_date) == -1:
+                        trash_model.insert_email(parsed_email_message)
+                    label_ids.remove(GMAIL_LABEL_TRASH)
+                    # And we have to make sure that it is not present in any other model
+                    for lbl in label_ids:
+                        model = self._get_model(lbl)
+                        if not model:
+                            continue
+                        idx = model.find_email(m.message_id, m.internal_date)
+                        if idx != -1:
+                            model.pop_email(m.message_id, idx)
                 else:
-                    LOG.info(f"Email with id={his_record.message_id} was already deleted "
-                             f"from the model with label_id={his_record.initial_label_id}")
-            elif action == HistoryRecord.ACTION_TRASH:
-                trash_model = self.registered_models.get(LABEL_ID_TRASH)
-                email = his_record.email
-                model.pop_email(email.get('id'))
-                trashed_idx = trash_model.find_email(email.get('id'), email.get('internalDate'))
-                if trashed_idx != -1:
-                    # This means that our app made this change, thereby we just have to
-                    # update the historyId.
-                    trash_model.get_item(trashed_idx)['historyId'] = his_record.history_id
-                    LOG.info(f"Email with id={email.get('id')} was already moved to trash from the "
-                             f"model with label_id={his_record.initial_label_id}. "
-                             f"Updating only historyId({his_record.history_id}).")
+                    LOG.warning("TRASH is NOT present in label_ids")
+                    # If TRASH is not in label_ids, then we can add the message to all matching
+                    # models, if it's not already there.
+                    for lbl in label_ids:
+                        model = self._get_model(lbl)
+                        if not model:
+                            continue
+                        if model.find_email(m.message_id, m.internal_date) == -1:
+                            model.insert_email(parsed_email_message)
+            # This should strictly process history records with only LABELS_ADDED and
+            # LABELS_REMOVED record types.
+            elif his.labels_modified():
+                LOG.warning("In HistoryRecord.labels_modified()")
+                # Remove message from all old places.
+                LOG.warning(f"labels_removed: {his.labels_removed}")
+                for lbl in his.labels_removed:
+                    model = self._get_model(lbl)
+                    if not model:
+                        continue
+                    idx = model.find_email(m.message_id, m.internal_date)
+                    if idx != -1:
+                        model.pop_email(m.message_id, idx)
+
+                label_ids = m.label_ids.split(',')
+                LOG.warning(f"label_ids: {label_ids}")
+                if GMAIL_LABEL_TRASH in label_ids:
+                    LOG.warning("TRASH is present in label_ids")
+                    # If message contains TRASH label, then we have to add it to trash model.
+                    trash_model = self._get_model(GMAIL_LABEL_TRASH)
+                    if trash_model.find_email(m.message_id, m.internal_date) == -1:
+                        trash_model.insert_email(parsed_email_message)
+                    label_ids.remove(GMAIL_LABEL_TRASH)
+                    # And we have to make sure that it is not present in any other model
+                    for lbl in label_ids:
+                        model = self._get_model(lbl)
+                        if not model:
+                            continue
+                        idx = model.find_email(m.message_id, m.internal_date)
+                        if idx != -1:
+                            model.pop_email(m.message_id, idx)
                 else:
-                    trash_model.insert_email(email)
-                    LOG.info(f"Email moved to the trash model from the model with "
-                             f"label_id={his_record.initial_label_id}.")
-            elif action == HistoryRecord.ACTION_RESTORE:
-                trash_model = self.registered_models.get(LABEL_ID_TRASH)
-                email = his_record.email
-                trash_model.pop_email(email.get('id'))
-                restored_idx = model.find_email(email.get('id'), email.get('internalDate'))
-                if restored_idx != -1:
-                    model.get_item(restored_idx)['historyId'] = his_record.history_id
-                    LOG.info(f"Email with id={email.get('id')} was already restored from the trash to "
-                             f"the model with label_id={his_record.initial_label_id}. "
-                             f"Updating only historyId({his_record.history_id}).")
-                else:
-                    model.insert_email(email)
-                    LOG.info(f"Email moved from the trash model to "
-                             f"the model with label_id={his_record.initial_label_id}.")
-            else:
-                raise ValueError(f"Unknown history record action: {action}")
+                    LOG.warning("TRASH is NOT present in label_ids")
+                    # If TRASH is not in label_ids, then we have 2 cases(because we know we already
+                    # have a record of it, otherwise we would end up in MESSAGE_ADDED):
+                    # 1.) If TRASH was removed from labels(aka. is in labels_removed), then we have
+                    # to add that message to all labels
+                    # 2.) Otherwise, we only have to add that message to labels in labels_added
+                    LOG.warning(f"labels_removed: {his.labels_removed}")
+                    if GMAIL_LABEL_TRASH in his.labels_removed:
+                        for lbl in label_ids:
+                            model = self._get_model(lbl)
+                            if not model:
+                                continue
+                            if model.find_email(m.message_id, m.internal_date) == -1:
+                                model.insert_email(parsed_email_message)
+                    else:
+                        LOG.warning(f"labels_added: {his.labels_added}")
+                        for lbl in his.labels_added:
+                            model = self._get_model(lbl)
+                            if not model:
+                                continue
+                            if model.find_email(m.message_id, m.internal_date) == -1:
+                                model.insert_email(parsed_email_message)
+
+        for model in self.registered_models.values():
+            model.check_loaded_data()
+
+        t2 = time.perf_counter()
+        LOG.warning(f"HISTORY RECORDS DISPATCH PERF: {t2 - t1}")
         LOG.info('-------- HISTORY RECORDS DISPATCHED --------')
+
+    def _get_model(self, label_id):
+        """History records may contain unknown Label IDs, this method takes care of errors."""
+        # TODO: If we fail to get a matching model, it might mean that our labels are
+        #  out of sync, thus we have to schedule a lebels_sync and reset the cache
+        #  of every model.
+        model = self.registered_models.get(label_id)
+        if model is None:
+            LOG.warning(f"Missing model for Label ID = {label_id}.")
+        return model
 
     @classmethod
     def get_instance(cls):
